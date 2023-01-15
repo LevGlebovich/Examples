c445e94576a5c3f69c6a2d5739fb2e21611dee8a
Sergio <cska2403@icloud.com>
2023-01-15 15:03:15 +0300

    Redact Example 009 Intro Array Example

--- Examples/Example009_IntroArray/Program.cs
+++ Examples/Example009_IntroArray/Program.cs
@@ -1,18 +1,21 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿//Функция(метод) возвращаемого типа
+int Max(int arg1, int arg2, int arg3)
 {
+//  Сравнение и определение максимального зачения из 3-х
     int result = arg1;
-    if (arg2> result) result = arg2;
-    if (arg3> result) result=arg3;
-    return result;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;//<==== Сохранение результата вычислений в методе
 }
-//              0   1   2   3   4   5   6   7   8
-int[] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
+//индексы массива(счт начинается с нуля)     
+//               0   1    2   3   4   5   6   7   8
+int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
 
 
 
 int max = Max(
-    Max(array[0], array[1], array[2]),
-    Max(array[3], array[4], array[5]), 
-    Max(array[6], array[7], array[8])
+    Max(array[0], array[1], array[2]),//<====сравнение первых 3-х индексов массива
+    Max(array[3], array[4], array[5]),//<====сравнение вторых 3-х индексов массива
+    Max(array[6], array[7], array[8])//<====сравнение последних 3-х индексов массива
 );
 System.Console.WriteLine(max);
